d9b28cfe930cce676f3b31e711f0ff748892ef12
rohit-Ironman <72317546+rohit-Ironman@users.noreply.github.com>
2021-01-10 13:30:27 -0700

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -64,7 +64,10 @@ class Game{
                          
                      }
                     
-                     
+                         textSize(25);
+                         fill("white");
+                         text("Player 1 :" +allPlayers.player1.score,50,50);
+                        text("Player 2 :" + allPlayers.player2.score, 50, 100);
                  
                  }
                 
@@ -101,7 +104,15 @@ class Game{
                  }
                  
                   if (player.index !== null) {
-                     //fill code here, to destroy the objects.
+                      for (var i = 0; i < fruitGroup.length; i++) {
+                          if (fruitGroup.get(i).isTouching(players)) {
+                              fruitGroup.get(i).destroy();
+                              player.score =player.score+1;
+                              player.update();
+                              
+                          }
+                          
+                      }
                   }
                 
 
